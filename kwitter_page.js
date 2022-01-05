3c169df018abf26891992fd77a309a6ec3542219
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2022-01-05 21:17:39 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -37,13 +37,13 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 console.log(firebase_message_id);
 console.log(message_data);
 name= message_data['name'];
-message= message_data['message'];
+message= message_data['Message'];
 like= message_data['like'];
 name_with_tag= "<h4>" + name + "<img class= 'user_tick' src='tick.png'></h4>";
 message_with_tag= "<h4 class='message_h4'>" + message + "</h4>";
 like_buttton= "<button class= 'btn btn-warning' id="+firebase_message_id+" value="+like+" onclick= 'updatelike(this.id)'>";
 span_with_tag= "<span class= 'glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
-
+console.log(message_with_tag);
 row= name_with_tag + message_with_tag + like_buttton + span_with_tag;
 document.getElementById("output").innerHTML+= row;
 
